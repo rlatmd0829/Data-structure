476c89ccab3aa700e423a6fbaae4993446c51f8f
DESKTOP-CO5BGP3\KIESRND <rlatmd0829@naver.com>
2020-11-17 16:54:45 +0900

    selection sort

---
+++ src/main/java/selectionSort.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+
+public class selectionSort {
+    static int []arr = new int[]{7, 6, 2, 4, 3, 9, 1};
+    public static void main(String[] args) {
+        selectionSort(arr);
+
+    }
+
+
+        public static void selectionSort(int[] arr) {
+            for (int i = 0; i < arr.length; i++) {
+                int stay = i;
+                for (int j = i + 1; j < arr.length; j++) {
+                    if (arr[stay] > arr[j]) stay = j;
+                }
+
+                int temp = arr[stay];
+                arr[stay] = arr[i];
+                arr[i] = temp;
+
+                selectionPrint(arr);
+
+            }
+        }
+        public static void selectionPrint(int[] arr) {
+            for (int i = 0; i < arr.length; i++) {
+                System.out.print(arr[i] + " ");
+            }
+            System.out.println();
+        }
+
+
+}
