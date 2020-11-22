7fb04436732485078abb28ade73ed3fad93e080a
DESKTOP-DTQV5MM\rlatm <rlatmd0829@naver.com>
2020-11-22 14:58:49 +0900

    heapSort add

--- src/main/java/selectionSort.java
+++ src/main/java/selectionSort.java
@@ -2,14 +2,16 @@ import java.util.Arrays;
 import java.util.Scanner;
 
 public class selectionSort {
-    static int []arr = new int[]{7, 6, 2, 4, 3, 9, 1};
+    static int []arr = new int[]{13, 5, 6, 14, 38, 33, 2};
     static int []sorted = new int[7];
     static int count;
     public static void main(String[] args) {
         //selectionSort(arr);
         //bubbleSort(arr);
-        insertSort(arr);
+        //insertSort(arr);
         //mergeSort(arr, 0, arr.length-1);
+        //quickSort(arr, 0, arr.length-1);
+        heapSort(arr);
     }
 
 
@@ -106,6 +108,66 @@ public class selectionSort {
             System.out.print((count+1)+" 단계 : ");
             print(arr);
             count++;
+        }
+
+        public static void quickSort(int[] arr, int left, int right){
+            int L = left;
+            int R = right;
+            int pivot = arr[(left + right) / 2];  // 피벗을 배열의 가운데 위치한 요소로 설정
+
+            while(L <= R){
+                while (arr[L] < pivot) L++; // 피벗 왼쪽에는 피벗보다 작은 원소들이 위치해야 하고, 큰 원소가 있다면 반복문을 나온다.
+                while (arr[R] > pivot) R--; // 피벗 오른쪽에는 피벗보다 큰 원소들이 위치해야 하고, 작은 원소가 있다면 반복문을 나온다.
+
+                if(L <= R){  // 반복문을 도중에 빠져나온 L에는 피벗보다 큰값이 들어 있고, 반복문을 도중에 빠져나온 R에는 피벗보다 작은 값이 들어있다.
+                    if(L != R){ // L 과 R이 같지 않은경우 스왑한다.
+                        swap(arr, L, R);
+                    }
+                    L++;
+                    R--;
+                }
+                System.out.print((count+1)+" 단계 : ");
+                print(arr);
+                count++;
+            }
+            // while문을 빠져나온경우는 L 과 R이 역전된 경우이다.
+            // 이 경우 남아 있는 부분 배열이 정렬이 안되잇을 경우가 있다. 다시 퀵정렬을 돌린다.
+            if(left < R) quickSort(arr, left, R);
+            if(L < right) quickSort(arr, L, right);
+
+        }
+
+        public static void heapSort(int[] arr){
+            heapify(arr, arr.length); // max heap으로 만든다. max 힙은 완전히 정렬된 상태는 아님 => 이런상태 ( 38, 14, 33, 5, 13, 6, 2)
+                                      // 그래서 밑에서 루트 노드와 맨 마지막 값을 바꾼후에 heapify를 해줘서 루트 노드에 올라온값에 위치를 찾아주기 위해 한번더 heapify를 해줘야함.
+
+            for(int i=arr.length-1; i>= 0; i--){ // max heap 을 배열로 만들기 위해 정렬
+                swap(arr, 0, i); // max heap은 루트 노드가 가장 크기 때문에 루트노드와 제일 마지막 값과 바꿔준다.
+                //heapify(arr, i); // 제일 마지막 값과 루트 노드를 바꿔준 뒤에는 루트 노드에 올라온 값에 위치를 찾아줘야 하기 때문에 heapify를 돌린다.
+            }
+
+            System.out.print("Quick Sort : ");
+            print(arr);
+        }
+
+        public static void heapify(int[] arr, int idx){ // heap을 만든다.
+            for(int i=1; i<idx; i++){
+                int child = i;
+
+                while(child > 0) { // child 값이 루트노드가 될때가지 계속 비교
+                    int parent = (child -1)/2;  // 부모노드 위치는 (자식-1)/2 이다.
+                    if(arr[child] > arr[parent]){  // 자식 노드에 값이 부모 노드보다 크면 스왑한다.
+                        swap(arr, child, parent);
+                    }
+                    child = parent; // 인덱스 0이 될때까지 자식 노드와 부모노드를 비교하기 위해  child에 parent 값(인덱스)를 넣는다.
+                }
+            }
+        }
+
+        public static void swap(int[] arr, int left, int right){
+            int temp = arr[left];
+            arr[left] = arr[right];
+            arr[right] = temp;
         }
 
         public static void print(int[] arr) {
