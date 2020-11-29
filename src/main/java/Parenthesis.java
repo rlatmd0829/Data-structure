b533cd989438fa7af895b75d3303f433566c9c95
DESKTOP-DTQV5MM\rlatm <rlatmd0829@naver.com>
2020-11-29 22:44:47 +0900

    갑자기 이상한 잡다한것들이 들어왔다

---
+++ src/main/java/Parenthesis.java
@@ -0,0 +1,38 @@
+import java.util.Scanner;
+import java.util.Stack;
+
+public class Parenthesis { // 맞췄다!!
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        sc.nextLine();
+
+        for(int i=0; i<a; i++){
+            Stack<Object> stack = new Stack<>();
+            char b[] = sc.nextLine().toCharArray();
+            int cnt = 0;
+            for(int j=0; j<b.length; j++){
+
+                if(b[j]==')'){
+                    cnt--;
+                    if(stack.empty()){
+                        break;
+                    }
+                    stack.pop();
+                }
+                else{
+                    stack.push(b[j]);
+                    cnt++;
+                }
+            }
+            if(cnt == 0) {
+                System.out.println("YES");
+            }
+            else{
+                System.out.println("NO");
+            }
+
+        }
+        sc.close();
+    }
+}
