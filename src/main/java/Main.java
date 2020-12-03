679c6b68b27b86d632e4ee9d15453336659d9e5f
DESKTOP-DTQV5MM\rlatm <rlatmd0829@naver.com>
2020-12-03 21:33:29 +0900

    백준 test에 문제풀이

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,17 +1,20 @@
-import java.io.*;
+
+
+
 import java.util.Scanner;
-import java.util.StringTokenizer;
+
 
 public class Main {
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args)  {
         Scanner sc = new Scanner(System.in);
-        int a = sc.nextInt();
-        int b = sc.nextInt();
-        System.out.println(a+b);
-        System.out.println(a-b);
-        System.out.println(a*b);
-        System.out.println(a/b);
-        System.out.println(a%b);
+        int A = sc.nextInt();
+        sc.nextLine();
+        String str = sc.nextLine();
 
+        System.out.println(A*(Character.getNumericValue(str.charAt(2))));
+        System.out.println(A*(str.charAt(1)-'0'));
+        System.out.println(A*(str.charAt(0)-'0'));
+        System.out.println(A*(Integer.parseInt(str)));
     }
 }
+
