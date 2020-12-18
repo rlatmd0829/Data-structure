5eb80ac14adfaf86cd36e02cbbd90ddc0cf9109b
DESKTOP-CO5BGP3\KIESRND <rlatmd0829@naver.com>
2020-12-18 17:16:43 +0900

    주석 정리

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -10,13 +10,6 @@ import java.util.StringTokenizer;
 
 public class Main {
     static public void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int N = sc.nextInt();
-
-        int x=1;
-        int y=1;
-
-
 
    }
 }
