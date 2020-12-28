bdb8f80bc5daa3d24ef33ddc7cc48902a9be4de7
DESKTOP-CO5BGP3\KIESRND <rlatmd0829@naver.com>
2020-12-28 17:03:26 +0900

    주석

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,22 +1,9 @@
-
-
-
-import java.io.*;
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Scanner;
-import java.util.StringTokenizer;
-
 
 public class Main {
     static public void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int N = sc.nextInt();
-
-
     }
-
-
-
 }
 
