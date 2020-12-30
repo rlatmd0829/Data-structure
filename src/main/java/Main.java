f57d204ec64947524b3957bc2416e164c8e9eee4
DESKTOP-CO5BGP3\KIESRND <rlatmd0829@naver.com>
2020-12-30 16:17:15 +0900

    20.12.30 백준문제풀이

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,10 +1,29 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.lang.reflect.Array;
+import java.util.*;
 
 public class Main {
-    static public void main(String[] args) {
+    static public void main(String[] args) throws IOException {
         Scanner sc = new Scanner(System.in);
-        int N = sc.nextInt();
 
+        int N = sc.nextInt();
+        int arr[] = new int[N];
+        int cnt[] = new int[4001];
+
+        int sum = 0;
+        for(int i=0; i<N; i++){
+            int a = sc.nextInt();
+            arr[i]=a;
+            sum += a;
+        }
+        Arrays.sort(arr);
+
+        System.out.println(sum/N);
+        System.out.println(arr[N/2]);
+        System.out.println();
+        System.out.println(arr[N-1]-arr[0]);
     }
 }
 
