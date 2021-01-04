8f3d7b67c0da032b90aec9356bd6983b9f2895f3
DESKTOP-CO5BGP3\KIESRND <rlatmd0829@naver.com>
2021-01-04 15:13:02 +0900

    21.01.04 백준

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -8,14 +8,9 @@ public class Main {
     static public void main(String[] args) throws IOException {
         Scanner sc = new Scanner(System.in);
 
-        int m = sc.nextInt();
-        int n = sc.nextInt();
+        int N = sc.nextInt();
+        int M = sc.nextInt();
 
-        for(int i=0; i<m; i++){
-            for(int j=0; j<n; j++){
-
-            }
-        }
 
     }
 
