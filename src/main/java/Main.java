f665defd8faaa07b42f3c8f588a024cc1afdc9a2
DESKTOP-CO5BGP3\KIESRND <rlatmd0829@naver.com>
2021-01-28 15:47:25 +0900

    21.01.28 알고리즘 공부

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -9,53 +9,30 @@ public class Main {
     public static void main(String[] args) throws IOException {
 
         Scanner sc = new Scanner(System.in);
-        //int n = sc.nextInt();
+        int n = sc.nextInt();
         int[] array = {1,5,2,6,3,7,4};
         int[][] commands = {{2,5,3},{4,4,1},{1,7,3}};
 
 
-        int[] abc = solution(array, commands);
-        System.out.println("[");
-        for(int a : abc){
 
-            System.out.print(a);
-            if()
-            System.out.println(", ");
-
-        }
-        System.out.println("]");
+        System.out.println(solution(n));
 
 
 
     }
-    public static int[] solution(int[] array, int[][] commands) {
-        int x=0,y=0,z=0;
-
-        int[] answer = {};
-        int [] result ={};
-        result = new int[commands.length];
-        for(int i=0; i<commands.length; i++){
-            int cnt = 0;
-            x = commands[i][0];
-            y = commands[i][1];
-            z = commands[i][2];
-
-            answer = new int[y-x+1];
-            for(int j=x; j<=y; j++){
-
-                answer[cnt] = array[j-1];
-                cnt++;
-
+    public String[] solution(String[] strings, int n) {
+        String[] answer = {};
+        for(int i=0; i<strings.length; i++){
+            String str = strings[i];
+            char[] c = str.toCharArray();
+            for(int j=0; j<c.length; j++){
+                int max = 0;
+                if(max < c[j]-'0'){
+                    max = c[j]-'0';
+                }
             }
-            Arrays.sort(answer);
-
-
-            result[i] = answer[z-1];
-
         }
-
-
-        return result;
+        return answer;
     }
     
 }
