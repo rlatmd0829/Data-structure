afc1a0aa7b85ef85c2bbeb7af9c0a16752b6e40f
DESKTOP-DTQV5MM\rlatm <rlatmd0829@naver.com>
2021-01-29 17:05:46 +0900

    21.01.29 알고리즘 공부

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -9,30 +9,40 @@ public class Main {
     public static void main(String[] args) throws IOException {
 
         Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
+        //int n = sc.nextInt();
         int[] array = {1,5,2,6,3,7,4};
         int[][] commands = {{2,5,3},{4,4,1},{1,7,3}};
 
 
-
-        System.out.println(solution(n));
+        System.out.println('a'-'0');
 
 
 
     }
-    public String[] solution(String[] strings, int n) {
-        String[] answer = {};
-        for(int i=0; i<strings.length; i++){
-            String str = strings[i];
-            char[] c = str.toCharArray();
-            for(int j=0; j<c.length; j++){
-                int max = 0;
-                if(max < c[j]-'0'){
-                    max = c[j]-'0';
-                }
+    boolean solution(String s) {
+
+        char c [] = s.toCharArray();
+
+        int pcnt=0;
+        int ycnt=0;
+        for(int i=0; i<c.length-1; i++){
+            if(c[i]=='p' || c[i]=='P'){
+                pcnt++;
+            }
+            else if(c[i] == 'y' || c[i] == 'Y'){
+                ycnt++;
             }
         }
-        return answer;
+        if(pcnt == ycnt){
+            return true;
+        }
+        else {
+            return false;
+        }
+        // [실행] 버튼을 누르면 출력 값을 볼 수 있습니다.
+        //System.out.println("Hello Java");
+
+
     }
     
 }
