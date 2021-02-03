c44ec2d0132e95d45276d180b3118f4719b664b7
DESKTOP-CO5BGP3\KIESRND <rlatmd0829@naver.com>
2021-02-03 16:03:39 +0900

    21.02.03 알고리즘 공부

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -15,18 +15,27 @@ public class Main {
 
 
         //String a[] = solution(1132548);
-        System.out.println('Z'+1);
+        solution(12345);
         //solution("abc",1);
         //97~122 소문자
         //65~90 대문자
 
     }
-    public static String solution(String s, int n) {
-        String answer = "";
-        char[] c = s.toCharArray();
-        for(int i=0; i<c.length; i++){
-            c[i] = (char)(c[i]+n);
-            answer += c[i];
+    public static Integer[] solution(long n) {
+        Integer[] answer = {};
+        String s = Long.toString(n);
+        String str[] = s.split("");
+        answer = new Integer[str.length];
+        for(int i=0; i<str.length; i++){
+            answer[i] = Integer.parseInt(str[i]);
+        }
+
+       
+
+        List<Integer> list = Arrays.asList(answer);
+        Collections.reverse(list);
+        for(int i=0; i<answer.length; i++){
+            answer[i] = list.get(i);
         }
 
         return answer;
