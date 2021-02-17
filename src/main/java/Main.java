bddee9ac769504f5c82dce49d30261c6d0adb1c0
DESKTOP-CO5BGP3\KIESRND <rlatmd0829@naver.com>
2021-02-17 17:46:47 +0900

    21.02.17 백준 알고리즘 공부

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,61 +3,14 @@ import java.math.BigInteger;
 import java.util.*;
 
 public class Main {
+    static int[] arr;
     public static void main(String[] args) throws IOException {
 
+
         Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
-        int k = sc.nextInt();
-
-        int[] arr = new int[n+1];
-
-        for(int i=0; i<=n; i++){
-            arr[i] = i;
+        while(sc.hasNextLine()){
+            String str = sc.nextLine();
+            System.out.println(str);
         }
-
-        for(int i=2; i<=n; i++){
-            if(arr[i] == 0){
-                continue;
-            }
-            for(int j=i*i; j<=n; j+=i){
-                int temp = arr[j];
-                arr[j] = 0;
-                k--;
-                if(k==0){
-                    System.out.println(temp);
-                    System.exit(0);
-                }
-
-            }
-        }
-
-    }
-    public int solution(int bridge_length, int weight, int[] truck_weights) {
-        int answer = 0;
-        Queue<Integer> q = new LinkedList();
-        int sum=0;
-        for(int t : truck_weights) {
-            while (true) {
-                if (q.isEmpty()) {
-                    q.add(t);
-                    answer++;
-                    sum += t;
-                    break;
-                }else if(q.size() == bridge_length){
-                    sum -= q.poll();
-                }else{
-                    if(sum + t > weight){
-                        q.add(0);
-                        answer++;
-                    }else{
-                        q.add(t);
-                        sum +=t;
-                        answer++;
-                        break;
-                    }
-                }
-            }
-        }
-        return answer+bridge_length;
     }
 }
