b6763e011163a01a1a73bddb7b3e495a35b6d218
DESKTOP-CO5BGP3\KIESRND <rlatmd0829@naver.com>
2021-02-24 17:40:18 +0900

    복습

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,33 +1,13 @@
-import java.io.*;
-import java.math.BigInteger;
-import java.util.*;
 
-public class Main {
-    public static void main(String[] args) throws IOException {
-
+public class Main{
+    public static void main(String[] args)
+    {
+        TestGeneric<String, Integer> generic = new TestGeneric<String, Integer>();
     }
-    public String solution(int[] numbers) {
-        String answer = "";
-        String[] str = new String[numbers.length];
-        for(int i=0; i<numbers.length; i++){
-            str[i] = String.valueOf(numbers[i]);
-        }
-        Arrays.sort(str, new Comparator<String>() {
-            @Override
-            public int compare(String o1, String o2) {
-                return (o2+o1).compareTo(o1+o2);
-            }
-        });
-
-        if(str[0].equals("0")){
-            return "0";
-        }
-        for(String a : str){
-            answer +=a;
-        }
-
-
-        return answer;
+    public static class TestGeneric<T, K>
+    {
+        public T sample;
+        public K sapmle2;
     }
 
 }
