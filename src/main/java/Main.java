e6d2df3daddbd04a47c39148dac053380394eebb
DESKTOP-DTQV5MM\rlatm <rlatmd0829@naver.com>
2021-02-25 20:37:43 +0900

    알고리즘 공부

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,13 +1,28 @@
+import java.util.Scanner;
 
 public class Main{
     public static void main(String[] args)
     {
-        TestGeneric<String, Integer> generic = new TestGeneric<String, Integer>();
+        int cnt1=0;
+        int cnt2=0;
+        Scanner sc = new Scanner(System.in);
+        String str = sc.next();
+        char[] c = str.toCharArray();
+        for(int i=0; i<c.length; i++){
+            if(c[i] == '('){
+                cnt1++;
+            }
+            else if (c[i] == ')'){
+                    cnt2++;
+                }
+            }
+        if(cnt1 == cnt2){
+            System.out.println("YES");
+
+        }else{
+            System.out.println("NO");
+        }
     }
-    public static class TestGeneric<T, K>
-    {
-        public T sample;
-        public K sapmle2;
-    }
+
 
 }
